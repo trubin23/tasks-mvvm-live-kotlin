5b4f6c6ccb2e856ab5ddd0af9c2936a2fe38dd48
Trubin Andrey <trubin23@gmail.com>
2019-01-18 23:59:32 +0500

    add TaskDetailActivity

---
+++ app/src/main/java/ru/trubin23/tasks_mvvm_live_kotlin/taskdetail/TaskDetailActivity.kt
@@ -0,0 +1,12 @@
+package ru.trubin23.tasks_mvvm_live_kotlin.taskdetail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TaskDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.taskdetail_act)
+    }
+}
