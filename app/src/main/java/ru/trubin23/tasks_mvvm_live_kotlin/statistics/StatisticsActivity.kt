342484d8e2f4b362fa1c0c5dd3758a85c185f248
trubin23 <trubin23@gmail.com>
2019-01-19 18:05:39 +0500

    add StatisticsActivity without implementation

---
+++ app/src/main/java/ru/trubin23/tasks_mvvm_live_kotlin/statistics/StatisticsActivity.kt
@@ -0,0 +1,13 @@
+package ru.trubin23.tasks_mvvm_live_kotlin.statistics
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import ru.trubin23.tasks_mvvm_live_kotlin.R
+
+class StatisticsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.statistics_act)
+    }
+}
