e8b445bca85c9579245323ee4794fbe4c0adcab5
Trubin Andrey <trubin23@gmail.com>
2019-01-19 23:55:39 +0500

    implementation getView function in TasksAdapter class

--- app/src/main/java/ru/trubin23/tasks_mvvm_live_kotlin/tasks/TasksAdapter.kt
+++ app/src/main/java/ru/trubin23/tasks_mvvm_live_kotlin/tasks/TasksAdapter.kt
@@ -1,17 +1,42 @@
 package ru.trubin23.tasks_mvvm_live_kotlin.tasks
 
+import android.databinding.DataBindingUtil
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import ru.trubin23.tasks_mvvm_live_kotlin.data.Task
+import ru.trubin23.tasks_mvvm_live_kotlin.databinding.TaskItemBinding
 
 class TasksAdapter(
         private var tasks: List<Task>,
         private val tasksViewModel: TasksViewModel
 ) : BaseAdapter() {
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getView(position: Int, view: View?, parent: ViewGroup?): View {
+        val binding: TaskItemBinding = if (view == null){
+            val inflater = LayoutInflater.from(parent?.context)
+
+            TaskItemBinding.inflate(inflater, parent, false)
+        } else {
+            DataBindingUtil.getBinding(view)!!
+        }
+
+        val  userActionsListener = object : TaskItemUserActionsListener{
+            override fun onCompleteChanged(task: Task, view: View) {
+            }
+
+            override fun onTaskClicked(task: Task) {
+            }
+        }
+
+        with(binding){
+            task = getItem(position)
+            listener = userActionsListener
+            executePendingBindings()
+        }
+
+        return binding.root
     }
 
     override fun getItem(position: Int) = tasks[position]
