2b1ef795c28c8ba43a51254c1911fdb9cdf1f0b7
Trubin Andrey <trubin23@gmail.com>
2019-01-20 23:25:56 +0500

    continue implementation TasksActivity class

--- app/src/main/java/ru/trubin23/tasks_mvvm_live_kotlin/tasks/TasksActivity.kt
+++ app/src/main/java/ru/trubin23/tasks_mvvm_live_kotlin/tasks/TasksActivity.kt
@@ -1,15 +1,20 @@
 package ru.trubin23.tasks_mvvm_live_kotlin.tasks
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.NavigationView
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
 import ru.trubin23.tasks_mvvm_live_kotlin.R
+import ru.trubin23.tasks_mvvm_live_kotlin.statistics.StatisticsActivity
 import ru.trubin23.tasks_mvvm_live_kotlin.util.addFragmentToActivity
 import ru.trubin23.tasks_mvvm_live_kotlin.util.setupActionBar
 
-class TasksActivity : AppCompatActivity() {
+class TasksActivity : AppCompatActivity(), TaskItemNavigator, TasksNavigator {
 
-    private lateinit var mDrawerLayout: DrawerLayout
+    private lateinit var drawerLayout: DrawerLayout
+
+    private lateinit var viewModel: TasksViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +38,30 @@ class TasksActivity : AppCompatActivity() {
     }
 
     private fun setupNavigationDrawer() {
+        drawerLayout = (findViewById<DrawerLayout>(R.id.drawer_layout)).apply {
+            setStatusBarBackground(R.color.colorPrimaryDark)
+        }
+        setupDrawerContent(findViewById(R.id.nav_view))
+    }
+
+    private fun setupDrawerContent(navigationView: NavigationView) {
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            if (menuItem.itemId == R.id.statistics_nav_menu_item){
+                val intent = Intent(this@TasksActivity, StatisticsActivity::class.java)
+                startActivity(intent)
+            }
+
+            drawerLayout.closeDrawers()
+
+            true
+        }
+    }
+
+    override fun openTaskDetails(taskId: String) {
+
+    }
+
+    override fun addNewTask() {
 
     }
 
