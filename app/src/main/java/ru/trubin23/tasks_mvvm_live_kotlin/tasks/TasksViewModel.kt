76362b71d2730a11a367be5ea431af8dd3d6484f
Trubin Andrey <trubin23@gmail.com>
2019-01-20 20:17:38 +0500

    continue implementation TasksViewModel class

--- app/src/main/java/ru/trubin23/tasks_mvvm_live_kotlin/tasks/TasksViewModel.kt
+++ app/src/main/java/ru/trubin23/tasks_mvvm_live_kotlin/tasks/TasksViewModel.kt
@@ -3,6 +3,9 @@ package ru.trubin23.tasks_mvvm_live_kotlin.tasks
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.content.Context
+import android.databinding.ObservableBoolean
+import android.databinding.ObservableField
+import android.graphics.drawable.Drawable
 import android.support.annotation.DrawableRes
 import android.support.annotation.StringRes
 import ru.trubin23.tasks_mvvm_live_kotlin.R
@@ -20,11 +23,33 @@ class TasksViewModel(
     val newTaskEvent = SingleLiveEvent<Void>()
     val snackbarMessage = SingleLiveEvent<Int>()
 
+    val currentFilteringLabel = ObservableField<String>()
+    val noTasksLabel = ObservableField<String>()
+    val noTaskIconRes = ObservableField<Drawable>()
+    val tasksAddViewVisible = ObservableBoolean()
+
+    val dataLoading = ObservableBoolean(false)
+    val isDataLoadingError = ObservableBoolean(false)
+
     var currentFiltering = TasksFilterType.ALL_TASKS
         set(value) {
             field = value
             updateFiltering()
         }
+
+    fun start(){
+        loadTasks(false)
+    }
+
+    fun loadTasks(forceUpdate: Boolean) {
+        loadTasks(forceUpdate, true)
+    }
+
+    private fun loadTasks(forceUpdate: Boolean, showLoadingUI: Boolean){
+        if (showLoadingUI){
+            //dataLoading.set(true)
+        }
+    }
 
     private fun updateFiltering() {
         when (currentFiltering) {
@@ -47,8 +72,10 @@ class TasksViewModel(
                           @StringRes noTasksLabelStringId: Int,
                           @DrawableRes noTaskIconDrawable: Int, tasksAddVisible: Boolean) {
         with(context.resources) {
-
+            currentFilteringLabel.set(getString(filteringLabelStringId))
+            noTasksLabel.set(getString(noTasksLabelStringId))
+            noTaskIconRes.set(getDrawable(noTaskIconDrawable))
+            tasksAddViewVisible.set(tasksAddVisible)
         }
     }
-
 }
