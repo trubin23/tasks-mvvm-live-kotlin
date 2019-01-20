3d69d60f5a8c2393d6f26c21ea55dded9a6c9c04
Trubin Andrey <trubin23@gmail.com>
2019-01-20 22:23:56 +0500

    finish filling task_frag layout

--- app/src/main/java/ru/trubin23/tasks_mvvm_live_kotlin/util/ViewExt.kt
+++ app/src/main/java/ru/trubin23/tasks_mvvm_live_kotlin/util/ViewExt.kt
@@ -1,8 +1,27 @@
 package ru.trubin23.tasks_mvvm_live_kotlin.util
 
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.Observer
+import android.databinding.BindingAdapter
 import android.support.design.widget.Snackbar
 import android.view.View
+import ru.trubin23.tasks_mvvm_live_kotlin.ScrollChildSwipeRefreshLayout
+import ru.trubin23.tasks_mvvm_live_kotlin.SingleLiveEvent
+import ru.trubin23.tasks_mvvm_live_kotlin.tasks.TasksViewModel
 
-fun View.showSnackBar(message: String, duration: Int){
+fun View.showSnackBar(message: String, duration: Int) {
     Snackbar.make(this, message, duration).show()
+}
+
+fun View.setupSnackbar(lifecycleOwner: LifecycleOwner,
+                       snackbarMessageLiveEvent: SingleLiveEvent<Int>, timeLenght: Int) {
+    snackbarMessageLiveEvent.observe(lifecycleOwner, Observer {
+        it?.let { showSnackBar(context.getString(it), timeLenght) }
+    })
+}
+
+@BindingAdapter("android:onRefresh")
+fun ScrollChildSwipeRefreshLayout.setSwipeRefreshLayoutOnRefreshListener(
+        viewModel: TasksViewModel) {
+    setOnRefreshListener { viewModel.loadTasks(true) }
 }
