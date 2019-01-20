ad75eec1d77cd192d04df90196f680e49497f211
Trubin Andrey <trubin23@gmail.com>
2019-01-20 19:52:49 +0500

    add SingleLiveEvent class

---
+++ app/src/main/java/ru/trubin23/tasks_mvvm_live_kotlin/SingleLiveEvent.kt
@@ -0,0 +1,32 @@
+package ru.trubin23.tasks_mvvm_live_kotlin
+
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
+import android.support.annotation.MainThread
+import java.util.concurrent.atomic.AtomicBoolean
+
+class SingleLiveEvent<T> : MutableLiveData<T>() {
+
+    private val pending = AtomicBoolean(false);
+
+    @MainThread
+    override fun observe(owner: LifecycleOwner, observer: Observer<T>) {
+        super.observe(owner, Observer<T> { t ->
+            if (pending.compareAndSet(true, false)) {
+                observer.onChanged(t)
+            }
+        })
+    }
+
+    @MainThread
+    override fun setValue(value: T?) {
+        pending.set(true)
+        super.setValue(value)
+    }
+
+    @MainThread
+    fun call() {
+        value = null
+    }
+}
--- app/src/main/java/ru/trubin23/tasks_mvvm_live_kotlin/tasks/TasksViewModel.kt
+++ app/src/main/java/ru/trubin23/tasks_mvvm_live_kotlin/tasks/TasksViewModel.kt
@@ -3,6 +3,10 @@ package ru.trubin23.tasks_mvvm_live_kotlin.tasks
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.content.Context
+import android.support.annotation.DrawableRes
+import android.support.annotation.StringRes
+import ru.trubin23.tasks_mvvm_live_kotlin.R
+import ru.trubin23.tasks_mvvm_live_kotlin.SingleLiveEvent
 import ru.trubin23.tasks_mvvm_live_kotlin.data.source.TasksRepository
 
 class TasksViewModel(
@@ -12,5 +16,39 @@ class TasksViewModel(
 
     private val context: Context = context.applicationContext
 
+    val openTaskEvent = SingleLiveEvent<String>()
+    val newTaskEvent = SingleLiveEvent<Void>()
+    val snackbarMessage = SingleLiveEvent<Int>()
+
+    var currentFiltering = TasksFilterType.ALL_TASKS
+        set(value) {
+            field = value
+            updateFiltering()
+        }
+
+    private fun updateFiltering() {
+        when (currentFiltering) {
+            TasksFilterType.ALL_TASKS -> {
+                setFilter(R.string.label_all, R.string.no_tasks_all,
+                        R.drawable.ic_verified, true)
+            }
+            TasksFilterType.ACTIVE_TASKS -> {
+                setFilter(R.string.label_active, R.string.no_tasks_active,
+                        R.drawable.ic_check_circle, false)
+            }
+            TasksFilterType.COMPLETED_TASKS -> {
+                setFilter(R.string.label_all, R.string.no_tasks_all,
+                        R.drawable.ic_check_box, false)
+            }
+        }
+    }
+
+    private fun setFilter(@StringRes filteringLabelStringId: Int,
+                          @StringRes noTasksLabelStringId: Int,
+                          @DrawableRes noTaskIconDrawable: Int, tasksAddVisible: Boolean) {
+        with(context.resources) {
+
+        }
+    }
 
 }
