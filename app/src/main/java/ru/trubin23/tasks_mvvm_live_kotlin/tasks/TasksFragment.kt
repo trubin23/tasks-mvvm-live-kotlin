d734ba8b89a0fb028755dcc2721e152d2ee4d4b0
Trubin Andrey <trubin23@gmail.com>
2019-01-21 23:23:03 +0500

    finish implementation TasksFragment class

--- app/src/main/java/ru/trubin23/tasks_mvvm_live_kotlin/tasks/TasksFragment.kt
+++ app/src/main/java/ru/trubin23/tasks_mvvm_live_kotlin/tasks/TasksFragment.kt
@@ -5,9 +5,8 @@ import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.PopupMenu
 import ru.trubin23.tasks_mvvm_live_kotlin.R
 import ru.trubin23.tasks_mvvm_live_kotlin.databinding.TasksFragBinding
 import ru.trubin23.tasks_mvvm_live_kotlin.util.setupSnackbar
@@ -58,15 +57,53 @@ class TasksFragment : Fragment() {
 
     private fun setupRefreshLayout() {
         viewDataBinding.refreshLayout.run {
-            val linkContext = context ?: return
-
             setColorSchemeColors(
-                    ContextCompat.getColor(linkContext, R.color.colorPrimary),
-                    ContextCompat.getColor(linkContext, R.color.colorAccent),
-                    ContextCompat.getColor(linkContext, R.color.colorPrimaryDark)
+                    ContextCompat.getColor(context ?: return, R.color.colorPrimary),
+                    ContextCompat.getColor(context ?: return, R.color.colorAccent),
+                    ContextCompat.getColor(context ?: return, R.color.colorPrimaryDark)
             )
 
             scrollUpChild = viewDataBinding.tasksList
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.tasks_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_clear -> {
+                viewDataBinding.viewmodel?.clearCompletedTasks()
+                true
+            }
+            R.id.menu_filter -> {
+                showFilteringPopMenu()
+                true
+            }
+            R.id.menu_refresh -> {
+                viewDataBinding.viewmodel?.loadTasks(true)
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun showFilteringPopMenu() {
+        PopupMenu(context ?: return, (activity ?: return).findViewById(R.id.menu_filter)).run {
+            menuInflater.inflate(R.menu.filter_tasks, menu)
+
+            setOnMenuItemClickListener {
+                viewDataBinding.viewmodel?.run {
+                    currentFiltering = when (it.itemId) {
+                        R.id.menu_active -> TasksFilterType.ACTIVE_TASKS
+                        R.id.menu_completed -> TasksFilterType.COMPLETED_TASKS
+                        else -> TasksFilterType.ALL_TASKS
+                    }
+                }
+                true
+            }
+            show()
         }
     }
 
